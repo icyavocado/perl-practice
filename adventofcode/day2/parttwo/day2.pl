23ece130a1832ec4388fad69d61d7c5f4ab6e09d
IcyAvocado <avocado@ovacoda.com>
2018-12-02 02:34:33 -0700

    Day 2: part 2

---
+++ adventofcode/day2/parttwo/day2.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $filename = 'input.txt';
+my (@list, @letter);
+open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
+while (<$fh>) {
+  chomp $_;
+  push @list, $_;
+}
+
+while (@list) {
+  my @id = split(//, shift @list);
+  foreach (@list) {
+    my @id_comp = split(//, $_);
+    my ($count, $char) = (0, undef);
+    for(my $i = 0; $i < scalar @id; $i++) {
+      if ($id[$i] ne $id_comp[$i]) {
+        $char = $i;
+        $count++;
+      }
+    }
+    splice @id, $char, 1 if $count == 1;
+    print join('', @id)."\n" if $count == 1
+  }
+}
