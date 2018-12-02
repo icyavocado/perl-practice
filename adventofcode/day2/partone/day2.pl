3779bef186f8921da3268374d41d4f0be4afff53
IcyAvocado <avocado@ovacoda.com>
2018-12-01 22:46:38 -0700

    UPDATE day 2 part 1

---
+++ adventofcode/day2/partone/day2.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Timer::Runtime;
+
+my $filename = 'input.txt';
+my (@list, $two, $three);
+open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
+while (<$fh>) {
+  chomp; push @list, $_;
+}
+
+foreach (@list) {
+  my %seen;
+  my $box_id = $_;
+  foreach my $letter (split(//, $box_id)) {
+    $seen{$letter}++;
+  }
+  foreach my $key (sort(keys %seen)) {
+    if ($seen{$key} == 2) { $two++; last; }
+  }
+  foreach my $key (sort(keys %seen)) {
+    if ($seen{$key} == 3) { $three++; last; }
+  }
+}
+
+print $two * $three."\n";
