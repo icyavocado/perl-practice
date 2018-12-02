9d1597636aea5e6ac3e65d001cb8292338706c45
IcyAvocado <avocado@ovacoda.com>
2018-12-02 02:59:34 -0700

    UPDATE Day 2 formating

--- adventofcode/day2/parttwo/day2.pl
+++ adventofcode/day2/parttwo/day2.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Timer::Runtime;
 
 my $filename = 'input.txt';
 my (@list, @letter);
@@ -15,13 +16,14 @@ while (@list) {
   foreach (@list) {
     my @id_comp = split(//, $_);
     my ($count, $char) = (0, undef);
-    for(my $i = 0; $i < scalar @id; $i++) {
+    for(my $i = 0; $i < scalar @id && $count > 1; $i++) {
       if ($id[$i] ne $id_comp[$i]) {
         $char = $i;
         $count++;
       }
     }
     splice @id, $char, 1 if $count == 1;
-    print join('', @id)."\n" if $count == 1
+    print join('', @id)."\n" if $count == 1;
+    last if $count == 1;
   }
 }
