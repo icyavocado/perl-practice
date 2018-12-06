e14fd13f3fb831b56b974dfac98eca6d20274e10
IcyAvocado <avocado@ovacoda.com>
2018-12-05 21:41:33 -0700

    FIX case when loop index when under 0

--- adventofcode/day5/alchemical_reduction.pl
+++ adventofcode/day5/alchemical_reduction.pl
@@ -1,46 +1,47 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Timer::Runtime;
 use Function::Parameters;
 use List::Util qw(min);
 
 my $filename = 'input.txt';
-open( my $fh, '<:encoding(UTF-8)', $filename )
-  or die "Could not open file '$filename' $!";
+open( my $fh, '<:encoding(UTF-8)', $filename );
 
-my $string;
+# Read the file
+my $string = <$fh>;
+
+# Alphabet a to z characters array
 my @alphabet = ( 'a' .. 'z' );
-my %seen;
 
-$string = <$fh>;
-
-print 'Part 1: ' . collapse() . "\n";
-
-foreach (@alphabet) {
-  $seen{$_} = scalar collapse($_);
-}
-
-print 'Part 2: ' . min( values %seen ) . "\n";
-
+# Reaction function
 fun collapse( $char = undef ) {
+
+  # Clone the string
   my $newstring = $string;
-  my @list = split( '', $newstring );
 
-  if ($char) {
-    $newstring =~ s/$char//g;
-    $char = uc $char;
-    $newstring =~ s/$char//g;
-    @list = split( '', $newstring );
-  }
+  # Remove upper character and lower character
+  $newstring =~ s/($char)|\U($char)//g if $char;
 
-  for ( my $i = 0; $i < scalar @list; $i++ ) {
-    last if !$list[ $i + 1 ];
+  # Make list out of the string
+  my @list = split( //, $newstring );
+
+  # Loop through the list
+  for ( my $i = 0; $i < scalar @list - 1; $i++ ) {
+
+    # If lowercase is the uppercase
     if ( abs ord( $list[$i] ) - ord( $list[ $i + 1 ] ) == 32 ) {
-      splice( @list, $i, 2 );
-      $i = $i - 2;
+
+      # Splice list at the index and the next one
+      splice @list, $i, 2;
+
+      # Move the $i back by 2
+      $i = $i > 0 ? $i - 2 : -1;
     }
   }
 
-  return @list;
+  # Return the count of the rest of the elements
+  return scalar @list;
 }
+
+print 'Part 1: ' . collapse() . "\n";
+print 'Part 2: ' . min( map { collapse($_) } @alphabet ) . "\n";
